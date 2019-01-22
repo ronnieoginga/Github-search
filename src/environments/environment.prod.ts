3848309fde6d909a43874acdd2ae5068aa325d60
ronnieoginga <oginga001ronney@gmail.com>
2019-01-22 10:31:35 +0300

    update readme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 
 export const environment = {
-  production: true
+  production: false,
+  apiUrl: ' https://api.github.com/users/',
+  apikey: '?access_token=ba411bd8aca12f6004d43cf52e21cb0be8e99324',
+  repoapikey: '/repos?access_token=ba411bd8aca12f6004d43cf52e21cb0be8e99324',
 };
